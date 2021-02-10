e9e48ffa019f5c07d54e7c8c3e0f5d6c732a17f2
omidaromak <78677128+omidaromak@users.noreply.github.com>
2021-02-10 17:43:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+"""
 
-## Modules
+ADC's Project"   _______________     "2021 winter"
 
+"""      
+#__________________________________________________________________#
+
+""" modules  """
 
 import numpy as np
 import math
@@ -8,9 +13,9 @@ import matplotlib.pyplot as plt
 from numpy import fft
 
     
+"""__________________________________________________________________"""
 
-
-## Initialization
+""" Initialization  """
 
 bpsk_6 = {
         "rate": 6,
@@ -77,8 +82,8 @@ qam64_54 = {
         "RATE": [0, 0, 1, 1],
     }
 
-mod = qam64_54
-snr = 15
+mod = bpsk_6
+snr = 5
 
 tail = np.zeros(6, dtype=int)
 
@@ -183,63 +188,66 @@ def test_decoder(inputd,rate):
     
     return codbit
 
-## interleaver
 
-def interleaver_Core (inp_vec, N_CBPS, N_BPSC):
-    s = max(N_BPSC/2,1)
+## interleaver abl
+def interleaver_Core(inp_vec, N_CBPS, N_BPSC):
+  s = max(N_BPSC/2,1)
 
-    inner_vec = np.ones(N_CBPS,dtype=np.int8)
-    for cnt in range(N_CBPS):
-        i = (N_CBPS/16) * (cnt % 16) + math.floor(cnt/16)
-        inner_vec[int(i)] = inp_vec[cnt]
+  inner_vec = np.ones(N_CBPS,dtype=np.int8)
+  for cnt in range(N_CBPS):
+    i = (N_CBPS/16) * (cnt % 16) + math.floor(cnt/16)
+    inner_vec[int(i)] = inp_vec[cnt]
+  
+  outp_vec = np.ones(N_CBPS,dtype=np.int8)
+  for cnt in range(N_CBPS):
+    j = (s * math.floor(cnt/s)) + ((cnt + N_CBPS - math.floor(16*cnt/N_CBPS)) % s)
+    outp_vec[int(j)] = inner_vec[cnt]
 
-    outp_vec = np.ones(N_CBPS,dtype=np.int8)
-    for cnt in range(N_CBPS):
-        j = (s * math.floor(cnt/s)) + ((cnt + N_CBPS - math.floor(16*cnt/N_CBPS)) % s)
-        outp_vec[int(j)] = inner_vec[cnt]
+  return outp_vec
 
-    return outp_vec
+def interleaver_a(inp_vec, N_CBPS, N_BPSC):
+  inp_len = len(inp_vec)
+  iteration = inp_len/N_CBPS
 
-def interleaver(inp_vec, N_CBPS, N_BPSC):
-    inp_len = len(inp_vec)
-    iteration = inp_len/N_CBPS
-
-    outp_vec = np.ones(inp_len,dtype=np.int8)
-    for cnt in range(int(iteration)):
-        vec = inp_vec[N_CBPS*cnt:N_CBPS*(cnt+1)]
-        outp_vec[N_CBPS*cnt:N_CBPS*(cnt+1)] = interleaver_Core(vec, N_CBPS, N_BPSC)
-
-    return outp_vec
+  outp_vec = np.ones(inp_len,dtype=np.int8)
+  for cnt in range(int(iteration)):
+    vec = inp_vec[N_CBPS*cnt:N_CBPS*(cnt+1)]
+    outp_vec[N_CBPS*cnt:N_CBPS*(cnt+1)] = interleaver_Core(vec, N_CBPS, N_BPSC)
+  
+  return outp_vec
 
 
 # __________________Deinterleaver unit __________________
 def deinterleaver_Core (inp_vec, N_CBPS, N_BPSC):
-    s = max(N_BPSC/2,1)
+  s = max(N_BPSC/2,1)
 
-    inner_vec = np.zeros(N_CBPS,dtype=np.int8)
-    for cnt in range(N_CBPS):
-        i = (s * math.floor(cnt/s)) + ((cnt + math.floor(16*cnt/N_CBPS)) % s)
-        inner_vec[int(i)] = inp_vec[cnt]
-
-    outp_vec = np.zeros(N_CBPS,dtype=np.int8)
-    for cnt in range(N_CBPS):
-        k = 16 * cnt - (N_CBPS-1) * math.floor(16*cnt/N_CBPS)
+  inner_vec = np.zeros(N_CBPS,dtype=np.int8)
+  for cnt in range(N_CBPS):
+    i = (s * math.floor(cnt/s)) + ((cnt + math.floor(16*cnt/N_CBPS)) % s)
+    inner_vec[int(i)] = inp_vec[cnt]
+  
+  outp_vec = np.zeros(N_CBPS,dtype=np.int8)
+  for cnt in range(N_CBPS):
+    k = 16 * cnt - (N_CBPS-1) * math.floor(16*cnt/N_CBPS)
     outp_vec[int(k)] = inner_vec[cnt]
 
-    return outp_vec
+  return outp_vec
 
-def deinterleaver(inp_vec, N_CBPS, N_BPSC):
+def deinterleaver_a(inp_vec, N_CBPS, N_BPSC):
 
-    inp_len = len(inp_vec)
-    iteration = inp_len/N_CBPS
+  inp_len = len(inp_vec)
+  iteration = inp_len/N_CBPS
 
-    outp_vec = np.ones(inp_len,dtype=np.int8)
-    for cnt in range(int(iteration)):
-        vec = inp_vec[N_CBPS*cnt:N_CBPS*(cnt+1)]
-        outp_vec[N_CBPS*cnt:N_CBPS*(cnt+1)] = deinterleaver_Core(vec, N_CBPS, N_BPSC)
+  outp_vec = np.ones(inp_len,dtype=np.int8)
+  for cnt in range(int(iteration)):
+    vec = inp_vec[N_CBPS*cnt:N_CBPS*(cnt+1)]
+    outp_vec[N_CBPS*cnt:N_CBPS*(cnt+1)] = deinterleaver_Core(vec, N_CBPS, N_BPSC)
 
-    return outp_vec
+  return outp_vec
 
+
+
+"""
 ###  interleaver ###
 
 def interleaver_a(data,Ncbps,Nbpsc):
@@ -270,7 +278,7 @@ def deinterleaver_a(data,Ncbps,Nbpsc):
 
     
 
-
+"""
 
 ## modulator
 
@@ -828,7 +836,7 @@ def awgn(Data,snr):
     import numpy as np
     import matplotlib.pyplot as plt
     import math
-    squared_numbers =  [abs(x) for x in Data]
+    squared_numbers =  [abs(x)**2 for x in Data]
     n=len(squared_numbers)
     f= sum(squared_numbers) / n# averaging of signal power
     z = np.random.normal(loc=0, scale=np.sqrt(2)/2, size=(n,2)).view(np.complex128) # random noise generating 
@@ -863,29 +871,38 @@ def find_mod(mod_rate):
     mod_rate =np.split(mod_rate, 4)
     if mod_rate == bpsk_6["RATE"]:
         mod_type = bpsk_6
-    if mod_rate == bpsk_9["RATE"]:
+    elif mod_rate == bpsk_9["RATE"]:
         mod_type = bpsk_9
-    if mod_rate == qpsk_12["RATE"]:
+    elif mod_rate == qpsk_12["RATE"]:
         mod_type = qpsk_12
-    if mod_rate == qpsk_18["RATE"]:
+    elif mod_rate == qpsk_18["RATE"]:
         mod_type = qpsk_18
-    if mod_rate == qam16_24["RATE"]:
+    elif mod_rate == qam16_24["RATE"]:
         mod_type = qam16_24
-    if mod_rate == qam16_36["RATE"]:
+    elif mod_rate == qam16_36["RATE"]:
         mod_type = qam16_36
-    if mod_rate == qam64_48["RATE"]:
+    elif mod_rate == qam64_48["RATE"]:
         mod_type = qam64_48
-    if mod_rate == qam64_54["RATE"]:
+    elif mod_rate == qam64_54["RATE"]:
         mod_type = qam64_54
+    else:
+        print("Due to extrem noise ,the reciver couldn't find out what is the coding rate")
+        exit()
     return mod_type
 
 def choose_packet(data,nop,iteration,ndbpp):
+    
+    "packet with 4095 OFDM symbol"
     complet_packet = data[i*ndbpp:(i+1)*ndbpp]
+
+    "packet with less than 4095 OFDM symbol"
     last_paacket = data[(nop-1)*ndbpp:]
+    
     if iteration<nop-1:
         output = complet_packet
     elif iteration==nop-1:
         output = last_paacket
+    
     return output
 
 
@@ -913,56 +930,120 @@ def delet_pad(p_data, pad_len):   ## data with pad
     return data
 
 def make_packet(chosen_packet, mod_type, tail, service,sfft):
-    sdatat = np.hstack((service, chosen_packet, tail))     ## [service chosen packet tail]
+    
+    " add service and tail "
+    sdatat = np.hstack((service, chosen_packet, tail))    
+    
+    " add pad "
     p_data,plen = add_pad(sdatat,mod_type)
+    
+    "scrambling data"
     s_data = scrambler(p_data,16,6)
+    
+    "encoding data"
     en_data = test_encoder(s_data, mod_type["coding rate"])   ##encoded data
+    
+    " interleaving "
     in_data = interleaver_a(en_data, mod_type["Ncbps"], mod_type["Nbpsc"])     ##interleaved data
+    
+    " modulation "
     mo_data = modulation(in_data,mod_type)      ##modulated data
+    
+    " make SIGNAL symbol "
     signal = make_signal(mod_type["RATE"], in_data, mod_type["Ncbps"])
+    
+    " add SIGNAL to the begining of the packet "
     adde_signal = np.hstack((signal,mo_data))
-    nos = int(len(adde_signal)/48)     ##number of symbol per packer
+    
+    " Calcualte number of OFDM symbol per packet "
+    nos = int(len(adde_signal)/48)    
+    
+    """ 
+    loop:
+    choose_symbol: choose modulated symbols for each OFDM symbol
+
+    """
     time_signal = np.array([])
     for i in range(nos):
         ch_symbol = choose_symbol(adde_signal, i)
-        all_symbol = subcarrier_allocation(ch_symbol)      ## allocated symbol
+        all_symbol = subcarrier_allocation(ch_symbol)     
         ifft_symbol = ifft_pilot(all_symbol,sfft)
         add_cp_symbol = add_CP(ifft_symbol)
         time_signal = np.append(time_signal,add_cp_symbol)
+    
+    " add preamble to the begining of the packet "
     time_signal = np.hstack((preamble, time_signal))
     return time_signal,plen
 
 def extract_packet(t_signal, mod_type, len_pad, sfft):     
-    adde_signal = []
+    
+    " delet preamble "
     t_signal = np.delete(t_signal,range(320))
+    
+    """
+     loop:
+     choose recieved symbol from each packet to get detected
+    """
+    adde_signal = []
     for i in range(int(len(t_signal)/80)):
         ch_r_symbol = choose_recieved_symbol(t_signal, i)
         rem_cp_symbol = remove_CP(ch_r_symbol)
         fft_symbol,pilot_value = fft_pilot(rem_cp_symbol, sfft) 
         exall_symbol = extract_data(fft_symbol)
         adde_signal = np.append(adde_signal,exall_symbol)
+    
+    """ detect rate ,
+     return modulated data in each packet,
+     calculate length of each packet"""
     mod_rate,packet_len,mo_data = extract_signal(adde_signal)
+    
+    " detect modulation type from detected rate "
     mod_type = find_mod(mod_rate)
+    
+    " demodulation of packet symbols"
     demo_data = demodulation(mo_data, mod_type)
+    
+    " deinterleaving "
     de_data = deinterleaver_a(demo_data, mod_type["Ncbps"], mod_type["Nbpsc"])
+    
+    " decoding "
     dec_data = test_decoder(de_data, mod_type["coding rate"]) 
+    
+    " descrambling "
     des_data = descrambler(dec_data,16)
+    
+    " remove pad"
     sdatat = delet_pad(des_data, len_pad)
+    
+    " remove tail and service and return transmited data bits of each packet "
     data = sdatat[len(service):len(sdatat)-len(tail)]
-
+    
     return data
 
+"_____________________________________________________START_________________________________________________ "
 
-source = np.random.randint(0, high=2, size=100000, dtype=int)
-nop = math.ceil(len(source)/(ndbppts))          # number of packets
+"Generating data"
+source = np.random.randint(0, high=2, size=10000, dtype=int)         
 
+"Calculating number of packet"
+nop = math.ceil(len(source)/(ndbppts)) 
+
+"Detected data"
 sourcehat = []
+
+"""
+Loop :  
+choose packet: calculate number of data bits for each packet
+make packet: make a time domain packet to send through channel
+extract packet : take each packet and try to detect the transmitted bits
+"""
 for i in range(nop):
-    ch_data = choose_packet(source,nop,i,ndbppts)       ## chosen packet   (bit)
-    t_signal,plen = make_packet(ch_data, mod, tail, service, sfft=64)    ## plen :pad length
+    ch_data = choose_packet(source,nop,i,ndbppts)       
+    t_signal,plen = make_packet(ch_data, mod, tail, service, sfft=64)    
     a_ch_signal = awgn(t_signal,snr)
     ex_data = extract_packet(a_ch_signal, mod, plen, sfft=64)
     sourcehat = np.append(sourcehat,ex_data) 
+   
     
 
 
