6ae5fe987eb1838ad8d47b2474403f1f302ea359
omidaromak <78677128+omidaromak@users.noreply.github.com>
2021-02-17 16:34:33 +0330

    Update subcarrier_allocation.py

--- subcarrier_allocation/subcarrier_allocation.py
+++ subcarrier_allocation/subcarrier_allocation.py
@@ -1,3 +1,5 @@
+###______Omid Abasi___9912344________###
+
 def subcarrier_allocation(mdata):         # mdata : modulated data 
     d_index = np.array([1, 2, 3, 4, 5, 6, 8, 9, 10,           # data index
             11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
